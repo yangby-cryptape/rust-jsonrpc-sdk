8e83eea957a6380e30ec550731b389cd8f891a51
Boyu Yang <yangby@cryptape.com>
2019-04-28 11:37:57 +0800

    feat: add a simple synchronous client

---
+++ client/src/async.rs
@@ -0,0 +1,93 @@
+// Copyright (C) 2019 Boyu Yang
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use futures::{future, Future};
+use reqwest::{
+    header::HeaderMap,
+    r#async::{
+        Client as RawClient, ClientBuilder as RawClientBuilder,
+        RequestBuilder as RawRequestBuilder, Response as RawResponse,
+    },
+    IntoUrl,
+};
+
+use jsonrpc_sdk_prelude::{jsonrpc_core::Response, CommonPart, Error, JsonRpcRequest, Result};
+
+pub struct Client(RawClient);
+pub struct ClientBuilder(RawClientBuilder);
+pub struct RequestBuilder(RawRequestBuilder);
+
+impl Client {
+    pub fn new() -> Self {
+        Client(RawClient::new())
+    }
+
+    pub fn builder() -> ClientBuilder {
+        ClientBuilder(RawClient::builder())
+    }
+
+    pub fn post<U>(&self, url: U) -> RequestBuilder
+    where
+        U: IntoUrl,
+    {
+        RequestBuilder(self.0.post(url))
+    }
+}
+
+impl Default for Client {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ClientBuilder {
+    pub fn build(self) -> Result<Client> {
+        Ok(Client(self.0.build()?))
+    }
+
+    pub fn tcp_nodelay(self) -> Self {
+        ClientBuilder(self.0.tcp_nodelay())
+    }
+
+    pub fn default_headers(self, headers: HeaderMap) -> Self {
+        ClientBuilder(self.0.default_headers(headers))
+    }
+
+    pub fn gzip(self, enable: bool) -> Self {
+        ClientBuilder(self.0.gzip(enable))
+    }
+
+    pub fn connect_timeout(self, timeout: ::std::time::Duration) -> Self {
+        ClientBuilder(self.0.connect_timeout(timeout))
+    }
+}
+
+impl RequestBuilder {
+    pub fn send<T>(
+        self,
+        content: T,
+        common: CommonPart,
+    ) -> impl Future<Item = T::Output, Error = Error>
+    where
+        T: JsonRpcRequest,
+    {
+        match content.to_single_request(common) {
+            Ok(request) => future::ok(request),
+            Err(error) => future::err(error),
+        }
+        .and_then(|request| {
+            self.0
+                .json(&request)
+                .send()
+                .and_then(RawResponse::error_for_status)
+                .and_then(|mut r| r.json::<Response>())
+                .map_err(std::convert::Into::into)
+                .and_then(T::parse_single_response)
+        })
+    }
+}
---
+++ client/src/sync.rs
@@ -0,0 +1,80 @@
+// Copyright (C) 2019 Boyu Yang
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+use reqwest::{
+    header::HeaderMap, Client as RawClient, ClientBuilder as RawClientBuilder, IntoUrl,
+    RequestBuilder as RawRequestBuilder, Response as RawResponse,
+};
+
+use jsonrpc_sdk_prelude::{jsonrpc_core::Response, CommonPart, JsonRpcRequest, Result};
+
+pub struct Client(RawClient);
+pub struct ClientBuilder(RawClientBuilder);
+pub struct RequestBuilder(RawRequestBuilder);
+
+impl Client {
+    pub fn new() -> Self {
+        Client(RawClient::new())
+    }
+
+    pub fn builder() -> ClientBuilder {
+        ClientBuilder(RawClient::builder())
+    }
+
+    pub fn post<U>(&self, url: U) -> RequestBuilder
+    where
+        U: IntoUrl,
+    {
+        RequestBuilder(self.0.post(url))
+    }
+}
+
+impl Default for Client {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl ClientBuilder {
+    pub fn build(self) -> Result<Client> {
+        Ok(Client(self.0.build()?))
+    }
+
+    pub fn tcp_nodelay(self) -> Self {
+        ClientBuilder(self.0.tcp_nodelay())
+    }
+
+    pub fn default_headers(self, headers: HeaderMap) -> Self {
+        ClientBuilder(self.0.default_headers(headers))
+    }
+
+    pub fn gzip(self, enable: bool) -> Self {
+        ClientBuilder(self.0.gzip(enable))
+    }
+
+    pub fn connect_timeout(self, timeout: ::std::time::Duration) -> Self {
+        ClientBuilder(self.0.connect_timeout(timeout))
+    }
+}
+
+impl RequestBuilder {
+    pub fn send<T>(self, content: T, common: CommonPart) -> Result<T::Output>
+    where
+        T: JsonRpcRequest,
+    {
+        content.to_single_request(common).and_then(|request| {
+            self.0
+                .json(&request)
+                .send()
+                .and_then(RawResponse::error_for_status)
+                .and_then(|mut r| r.json::<Response>())
+                .map_err(std::convert::Into::into)
+                .and_then(T::parse_single_response)
+        })
+    }
+}
